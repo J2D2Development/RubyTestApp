ea72ca4863677c0762ac9863c3c92b7cf8dd5451
J2D2Development <joedriscoll79@gmail.com>
2014-09-09 14:20:01 -0700

    Initial Rails Test Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '8a52cd2a38763dc08b3fef3cf85d6513343c1715a8dabddab6ed2c79b2884f08afb5226bc6d9317e496520f9bcff667f5359bf659d6e1f0653c3b87a1a76941e'
